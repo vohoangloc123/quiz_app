dded0c278711114db0d8e11b6cf27ad32167d58b
vohoangloc123 <you@example.com>
2024-08-10 16:18:51 +0700

    Added icon to button and applied opacity to logo

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -20,7 +20,9 @@ class StartScreen extends StatelessWidget {
             Image.asset(
               'assets/images/quiz-logo.png',
               width: 200,
+              color: Color.fromARGB(150, 255, 255, 255), //với 100 để làm độ mờ thay vì dùng Opacity để tránh giảm hiệu suất
             ),
+
             const SizedBox(
               height: 30,
             ),
@@ -31,14 +33,13 @@ class StartScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            ElevatedButton(
+              OutlinedButton.icon(
               onPressed: () {},
-              child: Text(
-                'Start quiz',
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepPurpleAccent),
+                icon: Icon(Icons.arrow_right_alt, color: Colors.white,),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.transparent), // Make border transparent
+                ),
+                label: const Text('Start Quiz', style: TextStyle(color: Colors.white),),
             )
           ],
         ),
