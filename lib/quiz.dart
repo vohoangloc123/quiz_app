f784d2e9f9ce521ce2575e99c834223b8fa7ea82
vohoangloc123 <you@example.com>
2024-08-10 18:14:08 +0700

    Use ternary expressions and comparison operators to handle page transitions

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -15,33 +14,31 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   static const startAlignment = Alignment.topLeft;
   static const endAlignment = Alignment.bottomRight;
-  Widget? activeScreen; //Biến này có kiểu Widget?, có thể là bất kỳ widget nào hoặc null.
-  @override
-  void initState() {
-    activeScreen=StartScreen(startQuiz: switchScreen);
-     super.initState();
-   }
+  var activeScreen =
+      'start-screen'; //Biến này có kiểu Widget?, có thể là bất kỳ widget nào hoặc null.
   void switchScreen() {
     setState(() {
-      activeScreen= const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-                Color.fromARGB(255, 33, 58, 87),
-                Color.fromARGB(255, 11, 100, 119),
-                Color.fromARGB(255, 20, 145, 155),
-                Color.fromARGB(255, 10, 209, 200),
-                Color.fromARGB(255, 69, 223, 177),
-                Color.fromARGB(255, 128, 237, 153),
-            ],begin: startAlignment, end: endAlignment)
-          ),
-          child: activeScreen,
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 33, 58, 87),
+            Color.fromARGB(255, 11, 100, 119),
+            Color.fromARGB(255, 20, 145, 155),
+            Color.fromARGB(255, 10, 209, 200),
+            Color.fromARGB(255, 69, 223, 177),
+            Color.fromARGB(255, 128, 237, 153),
+          ], begin: startAlignment, end: endAlignment)),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(startQuiz: switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
