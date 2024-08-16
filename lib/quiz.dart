11513baec6857ff7c2d4e4815b6dc619f54d9d98
vohoangloc123 <you@example.com>
2024-08-16 23:32:02 +0700

    removed debug banner

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -15,48 +15,55 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   static const startAlignment = Alignment.topLeft;
   static const endAlignment = Alignment.bottomRight;
-  List<String> selectedAnswers=[];
+  List<String> selectedAnswers = [];
   var activeScreen =
       'start-screen'; //Biến này có kiểu Widget?, có thể là bất kỳ widget nào hoặc null.
   void switchScreen() {
     setState(() {
-
       activeScreen = 'questions-screen';
     });
   }
+
   void resetQuiz() {
     setState(() {
-      selectedAnswers=[];
+      selectedAnswers = [];
       activeScreen = 'questions-screen';
     });
   }
 
-
   void chooseAnswer(String answer) {
-    selectedAnswers.add(answer); // Thêm câu trả lời vào danh sách câu trả lời đã chọn
-    if (selectedAnswers.length == questions.length) { // Kiểm tra nếu đã chọn câu trả lời cho tất cả các câu hỏi
+    selectedAnswers
+        .add(answer); // Thêm câu trả lời vào danh sách câu trả lời đã chọn
+    if (selectedAnswers.length == questions.length) {
+      // Kiểm tra nếu đã chọn câu trả lời cho tất cả các câu hỏi
       setState(() {
-        activeScreen = 'result-screen'; // Cập nhật trạng thái để chuyển sang màn hình bắt đầu hoặc kết thúc
+        activeScreen =
+            'result-screen'; // Cập nhật trạng thái để chuyển sang màn hình bắt đầu hoặc kết thúc
       });
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
               gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 70,0,144),
+            Color.fromARGB(255, 70, 0, 144),
             Color.fromARGB(255, 178, 108, 229),
           ], begin: startAlignment, end: endAlignment)),
           child: activeScreen == 'start-screen'
               ? StartScreen(startQuiz: switchScreen)
-              : activeScreen == 'questions-screen' ?QuestionsScreen(onSelectAnswer: chooseAnswer,)
-                : ResultsScreen(chosenAnswers: selectedAnswers, onResetQuiz: resetQuiz),
+              : activeScreen == 'questions-screen'
+                  ? QuestionsScreen(
+                      onSelectAnswer: chooseAnswer,
+                    )
+                  : ResultsScreen(
+                      chosenAnswers: selectedAnswers, onResetQuiz: resetQuiz),
         ),
       ),
     );
   }
 }
-
